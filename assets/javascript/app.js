621ae56b0f0ffbc4e2a1e3d8897b5c4b6aca9bb0
Hunter Rickert <hunter.rickert@gmail.com>
2019-07-25 16:03:23 -0700

    adding a bunch of js

---
+++ assets/javascript/app.js
@@ -0,0 +1,168 @@
+
+const myQuestions = [
+    {
+        question: "what is my favorite drink?",
+        answers: {
+            a: "soda",
+            b: "beer",
+            c: "wine"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What is the best site ever created?",
+        answers: {
+            a: "SitePoint",
+            b: "Simple Steps Code",
+            c: "Trick question; they're both the best"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Where is Waldo really?",
+        answers: {
+            a: "Antarctica",
+            b: "Exploring the Pacific Ocean",
+            c: "Sitting in a tree",
+            d: "Minding his own business, so stop asking"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Who is the strongest?",
+        answers: {
+            a: "Superman",
+            b: "The Terminator",
+            c: "Waluigi, obviously"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What is the best site ever created?",
+        answers: {
+            a: "SitePoint",
+            b: "Simple Steps Code",
+            c: "Trick question; they're both the best"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Where is Waldo really?",
+        answers: {
+            a: "Antarctica",
+            b: "Exploring the Pacific Ocean",
+            c: "Sitting in a tree",
+            d: "Minding his own business, so stop asking"
+        },
+        correctAnswer: "d"
+    }
+];
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+let counter = 15;
+let timer;
+
+(function () {
+function buildQuiz() {
+
+
+    // we'll need a place to store the HTML output
+    const output = [];
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+        // we'll want to store the list of answer choices
+        const answers = [];
+
+        // and for each available answer...
+        for (letter in currentQuestion.answers) {
+            // ...add an HTML radio button
+            answers.push(
+                `<label>
+          <input type="radio" name="question${questionNumber}" value="${letter}">
+          ${letter} :
+          ${currentQuestion.answers[letter]}
+
+        </label>`
+            );
+        }
+
+        // add this question and its answers to the output
+        output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+      <div class="answers"> ${answers.join("")} </div><div id="correctAnswer[${currentQuestion}]"></div>`
+      
+        );
+    });
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+}
+
+function showResults() {
+    clearInterval(timer);
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+        var correctChoice = currentQuestion.correctAnswer;
+        console.log(correctChoice);
+        $("#correctAnswer").text(correctChoice);
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        // if answer is correct
+        if (userAnswer === currentQuestion.correctAnswer) {
+            // add to the number of correct answers
+            numCorrect++;
+
+            // color the answers green
+            answerContainers[questionNumber].style.color = "lightgreen";
+        } else {
+            // if answer is wrong or blank
+            // color the answers red
+            answerContainers[questionNumber].style.color = "blue";
+        }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
+
+
+//display quiz right away
+//funtion to start the countdown, and decrease the time left
+function countdown() {
+    counter--;
+    $("#timer").text("Time Remaining: " + counter);
+
+    if (counter === 0) {
+        alert ("times up!");
+        clearInterval(timer);
+        showResults();
+    }
+}
+
+
+//function to start timer
+    function start () {
+        timer = setInterval(countdown, 1000);
+        $("#start").hide();
+    buildQuiz();
+    }
+//onclick function for start button
+$(document).on("click", "#start", function () {
+    console.log("start click working");
+    start();
+})
+// on submit, show results
+submitButton.addEventListener("click", showResults);
+})();
